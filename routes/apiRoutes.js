81784c7f7276f9ddd80d31f4c4f212460a9a6be8
Peter Geyser <peterpgeyser@gmail.com>
2019-10-06 16:22:17 -0700

    Users can now select a time that they need to complete their task by

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,21 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
+  // Get all tasks
   app.get("/api/tasks", function(req, res) {
     db.Todos.findAll({}).then(function(dbTasks) {
       res.json(dbTasks);
     });
   });
 
-  // Create a new example
+  // Create a new task
   app.post("/api/tasks", function(req, res) {
     db.Todos.create(req.body).then(function(dbTasks) {
       res.json(dbTasks);
     });
   });
 
-  // Delete an example by id
+  // Delete a task by id
   app.delete("/api/tasks/:id", function(req, res) {
     db.Todos.destroy({ where: { id: req.params.id } }).then(function(dbTasks) {
       res.json(dbTasks);
--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+HandlebarsIntl.registerWith(Handlebars);
+
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
