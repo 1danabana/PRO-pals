f1ac556fc0271f8dd8a667dfacb381f952b173d1
Dan <1danabana@gmail.com>
2019-10-09 21:22:52 -0700

    adding editing component of list

--- public/js/index.js
+++ public/js/index.js
@@ -147,6 +147,33 @@ var handleFormSubmit = function(event) {
   $taskCompleteBy.val("");
 };
 
+//Edit an existing task
+var editTask = function(){
+  console.log("Edit task...");
+
+
+  var listItem = this.parentNode;
+  var editInput = listItem.querySelector("input[type=text]");
+  var label = listItem.querySelector("label");
+
+  var containsClass = listItem.classList.contains("editMode");
+
+
+      //if the class pf parent is .editmode
+  if (containsClass){
+    //label text become the input's value  
+    label.innerText = editInput.value;
+
+  } else {
+      //switch to .editmode
+      //input value becomes the label's text
+    editInput.vaule = label.innerText;
+  }
+
+  listItem.classList.toggle("editMode"); //toggle .editmode on the parent
+
+}
+
 // handleDeleteBtnClick is called when an Task's delete button is clicked
 // Remove the Task from the db and refresh the list
 var handleDeleteBtnClick = function() {
